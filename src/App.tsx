aa8bf9a92653d098856bb04849143e87f63813ff
FrancyelZelling <zellingf@gmail.com>
2020-11-05 15:55:41 -0300

    Basic styling & navbar done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="">Hello World</h1>
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar: React.FC = () => {
+  return(
+  <nav>
+    <ul>
+      <li className="nav-item nav-item-focused">About Me</li>
+      <li className="nav-item">Projects</li>
+      <li className="nav-item">Contact</li>
+    </ul>
+  </nav>
+  );
+}
+
+export default Navbar;
